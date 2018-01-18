b7b6327e4f43f397f7700fb7e5e8c54b0e43d4a6
FBeggs <freya.beggs@students.plymouth.ac.uk>
2018-01-18 23:45:37 +0000

    JS exercise 4 attempt

--- Javascript_Examples/Exercise4/Exercise4Javascript.js
+++ Javascript_Examples/Exercise4/Exercise4Javascript.js
@@ -1,4 +1,20 @@
 function CreatePlayer()
 {
-
-}
+ var playerName = document.getElementById("playername").value;
+ var playerColour = document.getElementById("playercolour").value;
+ var playerHealth = document.getElementById("playerHealth").value;
+ var playerWeapon = document.getElementById("playerWeapon").value;
+ 
+ document.getElementById("name").innerHTML = playerName;
+ document.getElementById("colour").style.backgroundColor = playerColour;
+ document.getElementById("health").innerHTML = playerHealth;
+ 
+ if (playerWeapon == 1) {
+	document.getElementById("weapon").innerHTML = "Crossbow of nuch hurting";
+ }
+ else if (playerWeapon ==2) {
+	document.getElementById("weapon").innerHTML = "Broadsword of so slicing";
+ }
+ else {
+	document.getElementById("weapon").innerHTML = "Wand of amaze magics";
+ }
