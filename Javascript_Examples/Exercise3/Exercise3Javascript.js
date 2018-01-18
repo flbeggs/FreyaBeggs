b4c2a2087eea1849cfe0c894441b8756cceeefb7
FBeggs <freya.beggs@students.plymouth.ac.uk>
2018-01-18 23:22:49 +0000

    JS Exercise 3

--- Javascript_Examples/Exercise3/Exercise3Javascript.js
+++ Javascript_Examples/Exercise3/Exercise3Javascript.js
@@ -1,7 +1,18 @@
 var isLightOn = false;
 function ToggleLight()
 {
-	
+	if (isLightOn == true) {
+		document.getElementById("lightBulb").src = "img/bulb-on.png";
+		isLightOn = false;
+	}
+	else {
+		document.getElementById("lightBulb").src = "img/bulb-off.png";
+		isLightOn = true;
+	}
 }
 
+function AddNumbers(numA, numB)
+{
+	document.getElementById("numbers").innerHTML = (numA + " " + numB);
+}
 
