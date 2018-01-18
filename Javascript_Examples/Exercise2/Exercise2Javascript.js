6af9006e75219dbc4a88b877e13d9296238fe9ff
FBeggs <freya.beggs@students.plymouth.ac.uk>
2018-01-18 23:06:41 +0000

    Javascript exercise 2

--- Javascript_Examples/Exercise2/Exercise2Javascript.js
+++ Javascript_Examples/Exercise2/Exercise2Javascript.js
@@ -1,23 +1,23 @@
 
 function MakeTextSmaller()
 {
-	
+	document.getElementById("textSize").style.fontSize = "10px";
 }
 
 
 function ReplaceText()
 {
-	
+	document.getElementById("placeholderText").innerHTML = "My new text!";
 }
 
 
 
 function TurnLightOn()
 {
-	
+	document.getElementById("lightBulb").src = "img/bulb-on.png";
 }
 
 function TurnLightOff()
 {
-	
+	document.getElementById("lightBulb").src = "img/bulb-off.png";
 }
