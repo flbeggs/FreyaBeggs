c11dd88d88efbe3ff08c49e2368367a5880c23d3
FBeggs <freya.beggs@students.plymouth.ac.uk>
2018-01-18 22:45:18 +0000

    Javascript exercise 1

--- Javascript_Examples/Exercise1/Exercise1Javascript.js
+++ Javascript_Examples/Exercise1/Exercise1Javascript.js
@@ -1,16 +1,19 @@
 function AlertMessage()
 {
-
+alert("You have been Alerted!");
 }
 
 
 function ConsoleMessage()
 {
-
+console.log("Console is active");
 }
 
 
 function WhatsMyName()
 {
-  
+ var firstName = 'Jon';
+ var lastName = 'Brown';
+ 
+ console.log(firstName + " " + lastName);
 }
