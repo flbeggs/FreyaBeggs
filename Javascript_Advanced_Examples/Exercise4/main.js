1f1b4b95eaf1e3ab28fe71ec08fa3967a45dabef
FBeggs <freya.beggs@students.plymouth.ac.uk>
2018-01-19 01:37:04 +0000

    js ex 4

--- Javascript_Advanced_Examples/Exercise4/main.js
+++ Javascript_Advanced_Examples/Exercise4/main.js
@@ -5,5 +5,25 @@ function OnLoad()
 
 function SelectRoom(roomIndex)
 {
+	document.getElementById("roomTitle").innerHTML = roomArray[roomIndex].title;
+	document.getElementById("roomText").innerHTML = roomArray[roomIndex].text;
+	document.getElementById("roomChoices").innerHTML = "";
 	
+	var room = 0
+	
+	for (var i = 0; i < roomArray[roomIndex].choices.length; i++)
+	{
+		if (roomIndex == 0 || roomIndex == 2)
+		{
+			room = i + 1;
+		}
+		else
+		{
+			room = i + i;
+		}
+		
+		var theButton = "<button value='" + i + "' onClick=SelectRoom(" + (room) + ")>" + roomArray[roomIndex].choices[i].text + "</button>";
+		
+		document.getElementById("roomChoices").innerHTML += theButton;
+	}
 }
