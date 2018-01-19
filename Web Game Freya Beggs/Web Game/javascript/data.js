db9ec8d36b74bbea14c1737cbb4525f2482d600d
FBeggs <freya.beggs@students.plymouth.ac.uk>
2018-01-19 09:17:12 +0000

    whoops brackets

--- Web Game Freya Beggs/Web Game/javascript/data.js
+++ Web Game Freya Beggs/Web Game/javascript/data.js
@@ -1,7 +1,7 @@
 var roomArray = [
 	{
 		title:'Welcome', /* 0 */
-		text:'' ,
+		text:'',
 		image:'images/drow.png',
 		choices:[
 			{
@@ -63,7 +63,7 @@ var roomArray = [
 			}
 		]
 	},
-		{
+	{
 		title:'Ask for Watch',
 		text:'You consider who could keep watch through the night. Keeping watch yourself would avoid having to convince other members of the party. You’re unsure if you would be able to stop yourself from falling asleep abruptly, however. If you manage to persuade the other members of the party, the night-long watch could be divided into shifts. ',
 		image:'',
@@ -82,6 +82,7 @@ var roomArray = [
 			}
 		]
 	},
+	{
 		title:'In the Forest', /* 5 */
 		text:'You decide to head into the forest alone. Retrieving a torch from your pack, you stride towards the edge of the clearing, ignoring protests from your party members. You stumble around the forest for some time, unable to notice anything out of the ordinary. Realising the futility of the search, you begin to head back to camp. You must have travelled further than you realised, as the forest has become too dense to see the light of the campfire. Something rustles the leaves of the forest floor. Just as you turn to face the origin of the noise, a dark hooded figure is already approaching you.',
 		image:'',
@@ -96,6 +97,7 @@ var roomArray = [
 			}
 		]
 	},
+	{
 		title:'Ask Grigor for Help', /* 6 */
 		text:'Grigor seems unwilling at first, but you are able to persuade him after explaining yourself. He accompanies you into the forest.',
 		image:'',
@@ -106,6 +108,7 @@ var roomArray = [
 			}
 		]
 	},
+	{
 		title:'Ask Party for Help', /* 7 */
 		text:'You explain your suspicions to the party and ask if anyone wants to search with you. Only one person is willing to come with you; a scout for the party known as Miri. Her skills in tracking and archery will certainly come in handy. She accompanies you into the forest.',
 		image:'',
@@ -116,6 +119,7 @@ var roomArray = [
 			}
 		]
 	},
+	{	
 		title:'The Dream', /* 8 */
 		text:'Trapped within tight webbing in almost total darkness, you hear the hissing and clicking of a grotesque creature. Faint at first, but getting louder. The monster is upon you before you fully realise what’s happening.',
 		image:'',
@@ -126,6 +130,7 @@ var roomArray = [
 			}
 		]
 	},
+	{
 		title:'Then Nothing', /* 9 */
 		text:'You awaken with a jolt, shivering and coated in cold sweat. You see a long dark blade carve into the fabric of your tent effortlessly. In your last moments you see glimpses of obsidian skin and white flowing hair. A creature of the drow race.',
 		image:'',
@@ -136,6 +141,7 @@ var roomArray = [
 			}
 		]
 	},
+	{
 		title:'Escape', /* 10 */
 		text:'You run with all the energy you can muster.',
 		image:'',
@@ -146,6 +152,7 @@ var roomArray = [
 			}
 		]
 	},
+	{
 		title:'Captured!', /* 11 */
 		text:'You run with all the energy you can muster.',
 		image:'',
@@ -156,6 +163,7 @@ var roomArray = [
 			}
 		]
 	},
+	{
 		title:'Keep Watch', /* 12 */
 		text:'You overestimated your ability to remain awake. After what you think to be an hour or so of a surprisingly uneventful night, you are hit with an intense drowsiness. No matter how hard you try to fight it, your body begins to droop to the floor.',
 		image:'',
@@ -166,6 +174,7 @@ var roomArray = [
 			}
 		]
 	},
+	{
 		title:'Party Watch', /* 13 */
 		text:'Eventually you manage to convince some of the party members to keep watch. You go to your tent to sleep. You find drifting to sleep easy with the knowledge that the party is prepared for potential danger. <br /> <br /> A shout from a party member wakes you suddenly. Hearing the clattering of metal, you leave your tent to investigate. You see a party member trying to fend off a hooded figure.',
 		image:'',
@@ -180,6 +189,7 @@ var roomArray = [
 			}
 		]
 	},
+	{
 		title:'Grigor Watch', /* 14 */
 		text:'You eventually convince Grigor and he says he will keep watch for a while. You trust he will keep his word. You go to your tent to sleep. It’s difficult at first, but eventually you are able to force out your feelings of paranoia and drift off to sleep.',
 		image:'',
@@ -190,16 +200,21 @@ var roomArray = [
 			}
 		]
 	},
-		title:'In the Forest with Companion', /* 14 */
-		text:'You head into the forest with your companion.You stumble around the forest for some time, unable to notice anything out of the ordinary. Realising the futility of the search, you both begin to head back to camp. You must have travelled further than you realised, as the forest has become too dense to see the light of the campfire. Something rustles the leaves of the forest floor. Just as you turn to face the origin of the noise, a dark hooded figure is already approaching you. Your companion begins to draw their sword.',
+	{
+		title:'In the Forest with Companion', /* 15 */
+		text:'You head into the forest with your companion. You stumble around the forest for some time, unable to notice anything out of the ordinary. Realising the futility of the search, you both begin to head back to camp. You must have travelled further than you realised, as the forest has become too dense to see the light of the campfire. Something rustles the leaves of the forest floor. Just as you turn to face the origin of the noise, a dark hooded figure is already approaching you. Your companion begins to draw their sword.',
 		image:'',
 		choices:[
 			{
-				text:'The dream',
-				index:8
+				text:'Run',
+				index:10
+			},
+			{
+				text:'Fight',
+				index:11
 			}
 		]
-	},			
+	},		
 ]
 
 
