b3d0b2aeef6e07fb8cfb2074b3308ce887f1c581
FBeggs <freya.beggs@students.plymouth.ac.uk>
2018-01-19 00:32:33 +0000

    Wont work for some reason

--- Javascript_Advanced_Examples/Exercise1/main.js
+++ Javascript_Advanced_Examples/Exercise1/main.js
@@ -4,16 +4,22 @@ var weaponsArray = [
 	'Magic staff'
 ];
 
-
 function OnLoad()
 {
-	
-}
+	for (var i = 0; i <= 2; i++)
+	{
+		var weapon = "<option id='weapon'" + i + "value='0'>" + weaponsArray[i] + "</option>"
+		var selection = document.getElementById("weapons");
 
+		selection.innerHTML += weapon;
+	}
+}
 
 function SelectWeapon()
 {
-	
+	var display = document.getElementById("currentWeapon");
+	var selection = document.getElementById("weapons");
+	var selectOption = selection.options[selection.selectedIndex].text;
+
+	display.innerHTML = selectOption;
 }
-
-
