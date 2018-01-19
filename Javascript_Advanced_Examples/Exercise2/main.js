dbe6395623fcafb4192c7453df2053cb722ed759
FBeggs <freya.beggs@students.plymouth.ac.uk>
2018-01-19 01:11:08 +0000

    js ex 2

--- Javascript_Advanced_Examples/Exercise2/main.js
+++ Javascript_Advanced_Examples/Exercise2/main.js
@@ -1,6 +1,11 @@
-
 function StorePlayerData()
 {
+	var playerName = document.getElementById("playername").value;
+	localStorage.setItem("playername", playerName);
 	
+	var playerHealth = document.getElementById("playerhealth").value;
+	localStorage.setItem("playerhealth", playerHealth);
+	
+	var playerColour = document.getElementById("playercolour").value;
+	localStorage.setItem("playercolour", playerColour);
 }
-
