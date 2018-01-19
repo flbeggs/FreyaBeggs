f1eea7d469b6cde6f3bf89584e762f6c9c918140
FBeggs <freya.beggs@students.plymouth.ac.uk>
2018-01-19 00:01:04 +0000

    Actually works now

--- Javascript_Examples/Exercise4/Exercise4Javascript.js
+++ Javascript_Examples/Exercise4/Exercise4Javascript.js
@@ -1,20 +1,21 @@
 function CreatePlayer()
 {
- var playerName = document.getElementById("playername").value;
- var playerColour = document.getElementById("playercolour").value;
- var playerHealth = document.getElementById("playerHealth").value;
- var playerWeapon = document.getElementById("playerWeapon").value;
- 
- document.getElementById("name").innerHTML = playerName;
- document.getElementById("colour").style.backgroundColor = playerColour;
- document.getElementById("health").innerHTML = playerHealth;
- 
- if (playerWeapon == 1) {
-	document.getElementById("weapon").innerHTML = "Crossbow of nuch hurting";
+
+ document.getElementById("name").innerHTML = document.getElementById("playername").value;
+ document.getElementById("colour").style.backgroundColor = document.getElementById("playercolour").value;
+ document.getElementById("health").innerHTML = document.getElementById("playerhealth").value;
+ var option = document.getElementById("playerweapon").value;
+
+ if (option == 1)
+ {
+    document.getElementById("weapon").innerHTML = "Crossbow of much hurting";
  }
- else if (playerWeapon ==2) {
-	document.getElementById("weapon").innerHTML = "Broadsword of so slicing";
+ else if (option == 2)
+ {
+    document.getElementById("weapon").innerHTML = "Broadsword of so slicing";
  }
- else {
-	document.getElementById("weapon").innerHTML = "Wand of amaze magics";
+ else
+ {
+    document.getElementById("weapon").innerHTML = "Wand of amaze magics";
  }
+}
