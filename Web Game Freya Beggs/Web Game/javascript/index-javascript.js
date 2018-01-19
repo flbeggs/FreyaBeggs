221b65c230b6598b580b52866dc62fd83caa8c8d
FBeggs <freya.beggs@students.plymouth.ac.uk>
2018-01-19 01:48:23 +0000

    changes

--- Web Game Freya Beggs/Web Game/javascript/index-javascript.js
+++ Web Game Freya Beggs/Web Game/javascript/index-javascript.js
@@ -19,6 +19,11 @@ if (hourNow >= 18) {
 
 document.getElementById("hello").textContent = greeting;
 
+function OnLoad()
+{
+	SelectRoom(0);
+}
+
 /*
 Change Text with button Javascript
 */
@@ -58,3 +63,30 @@ function theBeginning() {
 	
 	
 }
+
+function SelectRoom(roomIndex)
+{
+	document.getElementById("roomTitle").innerHTML = roomArray[roomIndex].title;
+	document.getElementById("roomText").innerHTML = roomArray[roomIndex].text;
+	document.getElementById("roomChoices").innerHTML = "";
+	
+	var room = 0
+	
+	for (var i = 0; i < roomArray[roomIndex].choices.length; i++)
+	{
+		if (roomIndex == 0 || roomIndex == 2)
+		{
+			room = i + 1;
+		}
+		else
+		{
+			room = i + i;
+		}
+		
+		var theButton = "<button value='" + i + "' onClick=SelectRoom(" + (room) + ")>" + roomArray[roomIndex].choices[i].text + "</button>";
+		
+		document.getElementById("roomChoices").innerHTML += theButton;
+	}
+}
+
+
